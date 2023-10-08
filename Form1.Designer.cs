74e1eb650dc287196501e3cf1d21106c2e658556
MatMarrr <mateuszrmarek@gmail.com>
2023-10-08 23:57:40 +0200

    Add first homework

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace ISA_1
+{
+    partial class ISA
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonStart = new Button();
+            labelA = new Label();
+            inputA = new TextBox();
+            inputB = new TextBox();
+            inputN = new TextBox();
+            dropdownD = new ComboBox();
+            labelB = new Label();
+            labelD = new Label();
+            labelN = new Label();
+            dataGridView1 = new DataGridView();
+            lp = new DataGridViewTextBoxColumn();
+            xReal1 = new DataGridViewTextBoxColumn();
+            xInt1 = new DataGridViewTextBoxColumn();
+            xBin = new DataGridViewTextBoxColumn();
+            xInt2 = new DataGridViewTextBoxColumn();
+            xReal2 = new DataGridViewTextBoxColumn();
+            fX = new DataGridViewTextBoxColumn();
+            groupBox1 = new GroupBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(690, 16);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(75, 23);
+            buttonStart.TabIndex = 0;
+            buttonStart.Text = "Start";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // labelA
+            // 
+            labelA.AutoSize = true;
+            labelA.Location = new Point(21, 18);
+            labelA.Name = "labelA";
+            labelA.Size = new Size(13, 15);
+            labelA.TabIndex = 1;
+            labelA.Text = "a";
+            // 
+            // inputA
+            // 
+            inputA.Location = new Point(40, 16);
+            inputA.Name = "inputA";
+            inputA.Size = new Size(100, 23);
+            inputA.TabIndex = 2;
+            inputA.Text = "-4";
+            // 
+            // inputB
+            // 
+            inputB.Location = new Point(202, 16);
+            inputB.Name = "inputB";
+            inputB.Size = new Size(100, 23);
+            inputB.TabIndex = 3;
+            inputB.Text = "12";
+            // 
+            // inputN
+            // 
+            inputN.Location = new Point(542, 15);
+            inputN.Name = "inputN";
+            inputN.Size = new Size(100, 23);
+            inputN.TabIndex = 4;
+            inputN.Text = "10";
+            // 
+            // dropdownD
+            // 
+            dropdownD.DropDownStyle = ComboBoxStyle.DropDownList;
+            dropdownD.FormattingEnabled = true;
+            dropdownD.Items.AddRange(new object[] { "0,1", "0,01", "0,001", "0,0001" });
+            dropdownD.Location = new Point(353, 15);
+            dropdownD.Name = "dropdownD";
+            dropdownD.Size = new Size(121, 23);
+            dropdownD.TabIndex = 5;
+            // 
+            // labelB
+            // 
+            labelB.AutoSize = true;
+            labelB.Location = new Point(182, 19);
+            labelB.Name = "labelB";
+            labelB.Size = new Size(14, 15);
+            labelB.TabIndex = 6;
+            labelB.Text = "b";
+            // 
+            // labelD
+            // 
+            labelD.AutoSize = true;
+            labelD.Location = new Point(333, 19);
+            labelD.Name = "labelD";
+            labelD.Size = new Size(14, 15);
+            labelD.TabIndex = 7;
+            labelD.Text = "d";
+            // 
+            // labelN
+            // 
+            labelN.AutoSize = true;
+            labelN.Location = new Point(520, 19);
+            labelN.Name = "labelN";
+            labelN.Size = new Size(16, 15);
+            labelN.TabIndex = 8;
+            labelN.Text = "N";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { lp, xReal1, xInt1, xBin, xInt2, xReal2, fX });
+            dataGridView1.Location = new Point(21, 60);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(744, 343);
+            dataGridView1.TabIndex = 9;
+            dataGridView1.Visible = false;
+            // 
+            // lp
+            // 
+            lp.HeaderText = "lp.";
+            lp.Name = "lp";
+            lp.Width = 45;
+            // 
+            // xReal1
+            // 
+            xReal1.HeaderText = "x real";
+            xReal1.Name = "xReal1";
+            xReal1.Width = 60;
+            // 
+            // xInt1
+            // 
+            xInt1.HeaderText = "x int";
+            xInt1.Name = "xInt1";
+            xInt1.Width = 55;
+            // 
+            // xBin
+            // 
+            xBin.HeaderText = "x bin";
+            xBin.Name = "xBin";
+            xBin.Width = 58;
+            // 
+            // xInt2
+            // 
+            xInt2.HeaderText = "x int";
+            xInt2.Name = "xInt2";
+            xInt2.Width = 55;
+            // 
+            // xReal2
+            // 
+            xReal2.HeaderText = "x real";
+            xReal2.Name = "xReal2";
+            xReal2.Width = 60;
+            // 
+            // fX
+            // 
+            fX.HeaderText = "f(x)";
+            fX.Name = "fX";
+            fX.Width = 50;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(labelN);
+            groupBox1.Controls.Add(dataGridView1);
+            groupBox1.Controls.Add(buttonStart);
+            groupBox1.Controls.Add(labelA);
+            groupBox1.Controls.Add(labelD);
+            groupBox1.Controls.Add(inputA);
+            groupBox1.Controls.Add(labelB);
+            groupBox1.Controls.Add(inputB);
+            groupBox1.Controls.Add(dropdownD);
+            groupBox1.Controls.Add(inputN);
+            groupBox1.Location = new Point(12, 1);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(802, 426);
+            groupBox1.TabIndex = 10;
+            groupBox1.TabStop = false;
+            // 
+            // ISA
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(838, 450);
+            Controls.Add(groupBox1);
+            Name = "ISA";
+            Text = "ISA";
+            Resize += ISA_Resize;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button buttonStart;
+        private Label labelA;
+        private TextBox inputA;
+        private TextBox inputB;
+        private TextBox inputN;
+        private ComboBox dropdownD;
+        private Label labelB;
+        private Label labelD;
+        private Label labelN;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn lp;
+        private DataGridViewTextBoxColumn xReal1;
+        private DataGridViewTextBoxColumn xInt1;
+        private DataGridViewTextBoxColumn xBin;
+        private DataGridViewTextBoxColumn xInt2;
+        private DataGridViewTextBoxColumn xReal2;
+        private DataGridViewTextBoxColumn fX;
+        private GroupBox groupBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ISA_1
+{
+    public partial class ISA : Form
+    {
+        bool staticData = false;
+        public ISA()
+        {
+            InitializeComponent();
+            this.dropdownD.SelectedIndex = 2;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.AllowUserToAddRows = false;
+        }
+
+        public (double precision, int decimalPlaces) getDecimalPrecisionData()
+        {
+            string precision = this.dropdownD.SelectedItem.ToString()!;
+
+            switch (precision)
+            {
+                case "0,1":
+                    return (0.1, 1);
+                case "0,01":
+                    return (0.01, 2);
+                case "0,001":
+                    return (0.001, 3);
+                case "0,0001":
+                    return (0.0001, 4);
+                default:
+                    return (0, 0);
+            }
+        }
+
+        public int realToInt(double realNumber, int a, int b, int l)
+        {
+            return (int)Math.Floor((1 / (double)(b - a)) * (realNumber - a) * (Math.Pow(2, l) - 1));
+        }
+
+        public string intToBin(int intNumber, int l)
+        {
+            string binVal = Convert.ToString(intNumber, 2);
+            int missingBits = l - binVal.Length;
+
+            for (int i = 0; i < missingBits; ++i)
+            {
+                binVal = binVal.Insert(0, "0");
+            }
+
+            return binVal;
+        }
+
+        public int binToInt(string binNumber)
+        {
+            return Convert.ToInt32(binNumber, 2);
+        }
+
+        public double intToReal(int intNumber, int a, int b, int l, int decimalPlaces)
+        {
+            return Math.Round(((double)intNumber * (double)(b - a)) / (Math.Pow(2, l) - 1) + a, decimalPlaces);
+        }
+
+        public double GenerateNumberInRangeWithPrecision(int a, int b, int decimalPlaces)
+        {
+
+            Random random = new Random();
+
+            double number = random.NextDouble() * (b - a) + a;
+
+            return Math.Round(number, decimalPlaces);
+        }
+
+        public int getL(int a, int b, double d)
+        {
+            return (int)Math.Ceiling(Math.Log2((b - a) / d + 1));
+        }
+
+        public double xF(double x)
+        {
+            return (x % 1) * (Math.Cos(20 * Math.PI * x) - (Math.Sin(x)));
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // Clear data table
+            this.dataGridView1.Rows.Clear();
+
+            // Initialize variables
+            int a = 0, b = 0, n = 0;
+            var decimalPrecisionData = getDecimalPrecisionData();
+
+            // Set variables value
+            int.TryParse(this.inputA.Text, out a);
+            int.TryParse(this.inputB.Text, out b);
+            int.TryParse(this.inputN.Text, out n);
+            double d = decimalPrecisionData.precision;
+            int decimalPlaces = decimalPrecisionData.decimalPlaces;
+
+            if (staticData)
+            {
+                a = -2;
+                b = 3;
+                d = 0.001;
+                decimalPlaces = 3;
+            }
+            
+            int l = this.getL(a, b, d);
+
+            for (int i = 1; i <= n; ++i)
+            {
+                // Initialize variables
+                int xIntFirst, xIntSecond;
+                double xRealFirst, xRealSecond, xF;
+                string xBin;
+
+                // Set variables value
+                xRealFirst = this.GenerateNumberInRangeWithPrecision(a, b, decimalPlaces);
+                
+                if (staticData)
+                {
+                    xRealFirst = -1.012;
+                }
+
+                xIntFirst = this.realToInt(xRealFirst, a, b, l);
+                xBin = this.intToBin(xIntFirst, l);
+                xIntSecond = this.binToInt(xBin);
+                xRealSecond = this.intToReal(xIntSecond, a, b, l, decimalPlaces);
+                xF = this.xF(xRealSecond);
+
+                // Add table row with values
+                this.dataGridView1.Rows.Add(i, xRealFirst, xIntFirst, xBin, xIntSecond, xRealSecond, xF);
+            }
+
+            // Show table
+            this.dataGridView1.Visible = true;
+        }
+
+        private void ISA_Resize(object sender, EventArgs e)
+        {
+            this.groupBox1.Left = this.Width / 2 - this.groupBox1.Width / 2;
+        }
+    }
+}
